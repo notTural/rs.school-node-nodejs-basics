75a13a736a8f364298975608e2ab11573cbef239
notTural <rahman.trl.mammadov@gmail.com>
2025-04-29 01:40:28 +0200

    feat: implement hash functions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        const finalHash = hash.digest('hex');
+        console.log(finalHash);
+    });
+
+    stream.on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
 };
 
 await calculateHash();
